3ba1447290e185fd4b8c1f7c3621365f33c26695
Mo <martin@Air-M1.local>
2023-02-28 00:24:34 +0300

    curve connector done

--- rates_parser.py
+++ rates_parser.py
@@ -33,17 +33,18 @@ def get_token_decimals(token_address):
     '''
     This function assumes that the token is ERC20 standard.
     '''
+    # Load ERC20 ABI
     erc20_abi = json.load(open('erc20.abi.json'))
     decimals = w3.eth.contract(address=token_address,abi=erc20_abi).functions.decimals().call()
     return decimals
 
 
-def curve_connector(pool, token_in, token_out, swap_size) -> int:
+def curve_connector(pool, token_in, token_out, amount_in_human) -> int:
     '''
     :param pool: Address of the pool where the swap should happen
     :param token_in: Address of the token to be swapped (outgoing) -->
     :param token_out: Address of the token to be received (incoming) <--
-    :param swap_size: Size of the swap in normal units (this will be converted into the required decimal amount)
+    :param amount_in: Size of the swap in normal units (this will be converted into the required decimal amount)
     :return: int() output of exchange rate
     '''
 
@@ -54,7 +55,7 @@ def curve_connector(pool, token_in, token_out, swap_size) -> int:
 
     # Get ABI of the pool contract in order to interact with it.
     pool = w3.eth.contract(address=pool, abi=fetch_abi(pool)).functions
-    swap_size = swap_size * 10 ** (get_token_decimals(token_in))
+    amount_in = amount_in_human * 10 ** (get_token_decimals(token_in))
 
     # Make a list of tokens, which could be exchanged in the pool.
     coins = []
@@ -65,23 +66,25 @@ def curve_connector(pool, token_in, token_out, swap_size) -> int:
             pass
     print(coins)
 
-    # This function will be used to govern the logic of choosing an exchange rate
+    # This function will be used to calculate the given exchange rate
     if token_in and token_out in coins:
         print(f"Exchange rate can be calculated.\n")
-        output = pool.get_dy(coins.index(token_in),
-                             coins.index(token_out),
-                             swap_size).call()
-        print(output)
-
+        amount_out = pool.get_dy(coins.index(token_in),
+                                 coins.index(token_out),
+                                 amount_in).call()
+        amount_out_human = amount_out / 10 ** get_token_decimals(token_out)
+        print(f'Swap of {amount_in_human} of token {coins.index(token_in)} will yield {amount_out_human} '
+              f'of token {coins.index(token_out)}')
+        print(f'Exchange rate is {amount_out_human/amount_in_human}')
     else:
         print('Exchange rate cannot be calculated. Check if both tokens are contained in the pool.')
-    return output
+
 
 def main():
     curve_connector(pool='0xbebc44782c7db0a1a60cb6fe97d0b483032ff1c7',
                     token_in='0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
                     token_out='0xdAC17F958D2ee523a2206206994597C13D831ec7',
-                    swap_size=1000)
+                    amount_in_human=1000)
 
 
 if __name__ == "__main__":
